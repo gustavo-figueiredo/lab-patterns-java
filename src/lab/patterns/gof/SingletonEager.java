b8431a26e67a2f532443f7b5bba5853544e988d8
Gustavo Figueiredo <gf1985@gmail.com>
2024-08-28 12:50:08 -0300

    add SingletonEager

---
+++ src/lab/patterns/gof/SingletonEager.java
@@ -0,0 +1,15 @@
+package lab.patterns.gof;
+
+public class SingletonEager {
+
+    private static final SingletonEager instance = new SingletonEager();
+
+    private SingletonEager() {
+        super();
+    }
+
+    public static SingletonEager getInstance() {
+        return instance;
+    }
+
+}
